ce2875d2ad0dee2c6e7ed05ceb74fadbc0d31ed7
Scott Hulbert <scott@scotthulbert.com>
2014-07-30 18:54:58 -0700

    use list comprehensions & overhaul testing

--- piglatin.py
+++ piglatin.py
@@ -1,73 +1,104 @@
+import argparse
 import re
 import string
 import unittest
 
-class TestPigLatin(unittest.TestCase):
-	
-	def setUp(self):
-		self.vowels = ['a','e','i','o','u']
+class Translator():
+	VOWELS = 'aeiou'
 
 	def translate(self, english):
 		'''Takes full phrases and translates into piglatin'''
-
 		# finds groups of word and non-word items
 		chunks = re.finditer(r'(?P<word>\w+)|(?P<non_word>\W+)', english)
+		return ''.join([self._selective_translation(c) for c in chunks])
 
-		output = ""
-		for chunk in chunks:
-			match = chunk.groupdict()
-			if match['word'] is not None:
-				output += self._translate_word(match['word'])
-			else:
-				output += match['non_word']
-		return output.strip()
-		
+	def _selective_translation(self, chunk):
+		'''This only translates words, i.e. ignores non_words; chunk is a Python MatchObject'''
+		match = chunk.groupdict()
+		if match['word'] is not None:
+			return self._translate_word(match['word'])
+		else:
+			return match['non_word']
+
 	def _translate_word(self, word):
 		'''Only takes ascii character words'''
-		firstLetter = word[0]
-		lowerFirstLetter = word[0].lower()
+		translated_word = ''
+
+		first_vowel_index = next((i for i, c in enumerate(word) if c in self.VOWELS), None)
+
+		beginning_consonants = word[:first_vowel_index].lower()
+		rest_of_word = word[first_vowel_index:]
 		
-		returnStr = ""
-		
-		positionFirstVowel = -1
-		for i, c in enumerate(word):
-			if c in self.vowels:
-				positionFirstVowel = i
-				break
-		
-		consonantChunk = word[:positionFirstVowel].lower()
-		endChunk = word[positionFirstVowel:]
-		
-		if lowerFirstLetter not in self.vowels:
-			returnStr = endChunk + consonantChunk + "ay"
+		if word[0].lower() not in self.VOWELS:
+			translated_word = rest_of_word + beginning_consonants + 'ay'
 		else:
-			returnStr = word + "ay"
+			translated_word = word + "ay"
 
-		if firstLetter != lowerFirstLetter:
-			returnStr = returnStr[0].upper() + returnStr[1:]  # capitalizes first letter
+		if word[0].isupper():   # preserve capitalization of first letter
+			translated_word = translated_word[0].upper() + translated_word[1:]
 
-		return returnStr
+		return translated_word
 
-	def test_starting_consonant(self):
-		self.assertEqual(self.translate("hello"), "ellohay")
+class TranslatesCorrectly(unittest.TestCase):
+	KNOWN_RULES = [
+		{
+			'english': 'hello',
+			'piglatin': 'ellohay',
+			'description': 'single consonant moves to end with -ay'
+		},
+		{
+			'english': 'eat',
+			'piglatin': 'eatay',
+			'description': 'words starting with a vowel add -ay'
+		},
+		{
+			'english': 'hello apples',
+			'piglatin': 'ellohay applesay',
+			'description': 'simple phrase translates'
+		},
+		{
+			'english': 'Hello apples',
+			'piglatin': 'Ellohay applesay',
+			'description': 'first letter capitalization is preserved for consonant-beginning words'
+		},
+		{
+			'english': 'Eat apples',
+			'piglatin': 'Eatay applesay',
+			'description': 'first letter capitalization is preserved for vowel-beginning words'
+		},
+		{
+			'english': 'hello...  world?!',
+			'piglatin': 'ellohay...  orldway?!',
+			'description': 'punctuation is kept in place around words'
+		},
+		{
+			'english': 'school',
+			'piglatin': 'oolschay',
+			'description': 'words beginning with multiple consonants move ALL the consonants to the end'
+		}	
+	]
 
-	def test_starting_vowel(self):
-		self.assertEqual(self.translate("eat"), "eatay")
+	def __init__(self, english, piglatin, description=None):
+		super(TranslatesCorrectly, self).__init__()
+		self.english, self.piglatin, self.description = (english, piglatin, description)
+		self.translator = Translator()
 
-	def test_hello_apples(self):
-		self.assertEqual(self.translate("hello apples"), "ellohay applesay")
+	def runTest(self):
+		result = self.translator.translate(self.english)
+		description = "{0}\ndesired: {1} --> {2}\nactual: {1} --> {3}".format(
+			self.description, self.english, self.piglatin, result
+		)
+		self.assertEqual(result, self.piglatin, description)
 
-	def test_uppercase_phrase_consonant(self):
-		self.assertEqual(self.translate("Hello apples"), "Ellohay applesay")
-
-	def test_uppercase_phrase_vowel(self):
-		self.assertEqual(self.translate("Eat apples"), "Eatay applesay")
-		
-	def test_punctuation_phrase(self):
-		self.assertEqual(self.translate("hello...  world?!"), "ellohay...  orldway?!")
-	
-	def test_multi_consonant(self):
-		self.assertEqual(self.translate("school"), "oolschay")
-	
 if __name__ == "__main__":
-	unittest.main()
+	parser = argparse.ArgumentParser(description='Translate English phrases to piglatin')
+	parser.add_argument('-t','--text', type=str, required=False, default=None)
+	args = parser.parse_args()
+
+	suite = unittest.TestSuite()
+	suite.addTests([TranslatesCorrectly(**rule) for rule in TranslatesCorrectly.KNOWN_RULES])
+	unittest.TextTestRunner().run(suite)
+		
+	if args.text is not None:
+		translator = Translator()
+		print(translator.translate(args.text.strip()))
